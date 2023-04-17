78a811fe217ca1b907b715e70b84c3f14cc72cf6
Suelen Critina Vaz <sucris12@gmail.com>
2023-04-17 15:14:48 -0300

    Show the first screen in device. - Addtion swiftLint in app.

--- meuspensamentos/meuspensamentos/AppDelegate.swift
+++ meuspensamentos/meuspensamentos/AppDelegate.swift
@@ -3,14 +3,12 @@
 //  meuspensamentos
 //
 //  Created by Suh on 17/04/23.
-//
+// swiftlint:disable line_length
 
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
@@ -18,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     }
 
     // MARK: UISceneSession Lifecycle
-
     func application(_ application: UIApplication, configurationForConnecting connectingSceneSession: UISceneSession, options: UIScene.ConnectionOptions) -> UISceneConfiguration {
         // Called when a new scene session is being created.
         // Use this method to select a configuration to create the new scene with.
@@ -31,6 +28,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
 
-
 }
-
